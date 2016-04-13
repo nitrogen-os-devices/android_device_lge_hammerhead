072196a2c1ed7a58730e88fdb3b84751516cb123
Valeriy <binder_z@mail.ru>
2016-04-13 15:42:42 +0400

    Add post_boot service

---
+++ configs/init.qcom.post_boot.sh
@@ -0,0 +1,21 @@
+#!/system/bin/sh
+
+#
+# Copyright 2016 Nitrogen Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# init.qcom.post_boot.sh
+# Here it is possible to write any shell a code.
+# For example, to make verification of files if there is a file that to delete it.
+#
--- device.mk
+++ device.mk
@@ -41,6 +41,8 @@ PRODUCT_COPY_FILES := \
     $(LOCAL_KERNEL):kernel
 
 PRODUCT_COPY_FILES += \
+    device/lge/hammerhead/configs/init.qcom.post_boot.sh:system/bin/init.qcom.post_boot.sh \
+    device/lge/hammerhead/configs/96-postboot.sh:system/addon.d/96-postboot.sh \
     device/lge/hammerhead/init.hammerhead.rc:root/init.hammerhead.rc \
     device/lge/hammerhead/init.hammerhead.usb.rc:root/init.hammerhead.usb.rc \
     device/lge/hammerhead/fstab.hammerhead:root/fstab.hammerhead \
