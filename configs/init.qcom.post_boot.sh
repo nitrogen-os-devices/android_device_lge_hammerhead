7e8b971e20522eba6c0271b5bf47fc82037cdd9b
Valeriy <mexanicteam@gmail.com>
2016-09-17 19:58:55 +0400

    init.qcom.post_boot: more settings
    Change cpu governor
    Enable all cores
    Use 3072 kb read-ahead buffer size
    Set-up cpu freq min/max
    Run other scripts

--- configs/init.qcom.post_boot.sh
+++ configs/init.qcom.post_boot.sh
@@ -19,3 +19,40 @@
 # Here it is possible to write any shell a code.
 # For example, to make verification of files if there is a file that to delete it.
 #
+
+# Disble mpdecision
+stop mpdecision;
+echo -n 0 > /sys/kernel/msm_mpdecision/conf/boost_enabled
+
+# Enable all cores
+echo -n 1 > /sys/devices/system/cpu/cpu1/online
+echo -n 1 > /sys/devices/system/cpu/cpu2/online
+echo -n 1 > /sys/devices/system/cpu/cpu3/online
+
+# Set cpu governor
+echo -n "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo -n "ondemand" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+echo -n "ondemand" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+echo -n "ondemand" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+
+# Set cpu min freq
+echo -n 300000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo -n 300000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
+echo -n 300000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
+echo -n 300000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
+
+# Set cpu max freq
+echo -n 2265000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo -n 2265000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+echo -n 2265000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+echo -n 2265000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
+
+# Set read-ahead buffer size
+echo -n 3072 > /sys/devices/virtual/bdi/179:0/read_ahead_kb
+
+# Run other scripts
+if [ -f /system/etc/nitrogen.sh ]; then
+	# Alee 2 mins after boot
+	sleep 120
+	shell /system/etc/nitrogen.sh
+fi;
